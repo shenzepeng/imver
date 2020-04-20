50a0c3cd207734f2f7ce4205b3f061ca9e4b6a95
mac <806871609@qq.com>
2020-04-20 22:57:02 +0800

    init

--- src/main/java/com/example/imver/handler/HashHandler.java
+++ src/main/java/com/example/imver/handler/HashHandler.java
@@ -4,7 +4,7 @@ package com.example.imver.handler;
 /**
  * @Auther: szp
  * @Date: 2020/4/16 10:39
- * @Description: 沈泽鹏写点注释吧
+ * @Description: 散列处理器
  */
 public interface HashHandler {
     String getKeyToHashValue(String key);
